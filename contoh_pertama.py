adb0efa8d1e9dd9cf89cc3d9fc75be3195dd28fd
abirpl1 <abirpl742@gmail.com>
2026-08-21 07:06:48 +0700

    Add files via upload

---
+++ contoh_pertama.py
@@ -0,0 +1,13 @@
+#ini adalah komentar _tidak dijalankan oleh python
+#program: salam perkenalan
+
+print("halo, dunia!")              # menampilkan teks
+print("nama saya: Ahmad sabiin asifatusena")      # setiap print () = satu baris output
+print("versi: 3.x")       #tanda kutip bisa ganda atau tunggal
+
+print('halo, dunia~')
+print('belajar python')
+print("10 + 5")
+print('jumlah:', 10 + 5)
+print() #kosong
+print("A","B","C",sep="-")
