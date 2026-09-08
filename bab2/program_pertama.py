88db6c0ae6e668f6afad7ee5b9e0ca108772c838
abirpl1 <abirpl742@gmail.com>
2026-09-08 17:05:36 +0700

    Add files via upload

---
+++ bab2/program_pertama.py
@@ -0,0 +1,11 @@
+#program pertamaku
+#nama: ahmad sabiin asifatusena
+#kelas: X RPL 1
+
+print("====================================")
+print("     selamat datang di python!      ")
+print("====================================")
+print()
+print("program ini dinuat oleh:", "[abi]")
+print("sekolah: smk tjp tuban")
+print("tahun :", 2026)
